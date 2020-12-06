1a1d7d22260a8c35c6817a08165ea9a4d5f7e53f
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2020-12-06 18:14:17 -0300

    Modal config interface

---
+++ SRC/src/app/models/interfaces/ModalConfig.interface.ts
@@ -0,0 +1,8 @@
+export interface ModalConfig {
+  hasModalHead?: boolean;
+  hasModalFoot?: boolean;
+  title?: string;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+  show: boolean;
+}
