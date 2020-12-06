6f7604d1aa27720b14d7e7da814720a87b65d811
Mateus da Silveira Costa <mateus.costa98.dev@gmail.com>
2020-12-06 20:23:52 -0300

    Participants list in service is private

--- SRC/src/app/services/participants.service.ts
+++ SRC/src/app/services/participants.service.ts
@@ -11,7 +11,7 @@ export class ParticipantsService {
   /**
    * Participants mock list
    */
-  participants = [
+  private participants = [
     {
       id: Guid.create(),
       name: 'Humans',
